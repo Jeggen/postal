b858bf2f7a39fd849e0788ff6332e5f4366ff540
YOUR NAME <YOUR@EMAIL.com>
2016-06-16 15:25:37 +0530

    User's posts page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :posts
+
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
